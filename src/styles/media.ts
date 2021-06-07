4608544780707ce42f146b9d83fafc37a1ee01a3
tyral <tyral@zklink.org>
2021-06-07 11:44:09 +0800

    feat(styled): define media rule

---
+++ src/styles/media.ts
@@ -0,0 +1,17 @@
+
+export const size = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  xxl: '1536px',
+}
+
+export const device = {
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`,
+};
+
