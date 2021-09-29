7207d9b87378db287d13c3426b3f95de5f5bbd50
tyral <tyral@zklink.org>
2021-09-29 16:22:08 +0800

    refactor: redirect to test.zk.link

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "modern-normalize/modern-normalize.css";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div></div>,
   document.getElementById('root')
 );
 
